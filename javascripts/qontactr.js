25e879b329ac1bfc0797fb28dcc023abe061bba8
Frank Matranga <Apexal@users.noreply.github.com>
2016-12-07 08:57:44 -0500

    Separated Qontactr javascript

---
+++ javascripts/qontactr.js
@@ -0,0 +1,30 @@
+$(() => {
+  const container = $('.container');
+  const JSONstring = window.location.href.split('?data=')[1];
+  let decoded = decodeURIComponent(JSONstring);
+  const lastCommaIndex = decoded.lastIndexOf(',');
+  decoded = decoded.substring(0, lastCommaIndex) + decoded.substring(lastCommaIndex + 1);
+  let data = {};
+  try {
+    data = JSON.parse(decoded);
+    console.log(data);
+  } catch(err) {
+    alert('Failed to parse url!');
+    $('.status').text('Failed to load data!');
+    return;
+  }
+  $('.status').hide();
+  // Display info
+  for (let propName in data) {
+     // Finds proper element by property name and sets its proper text
+    const item = $('<div>', {
+      class: 'property',
+      'data-property-name': propName
+    });
+    const b = $('<b>', { class: 'property-name', text: propName.charAt(0).toUpperCase() + propName.slice(1) + ': ' });
+    const span = $('<span>', { class: 'property-value', text: data[propName] });
+    item.append(b);
+    item.append(span);
+    container.append(item);
+  }
+});
